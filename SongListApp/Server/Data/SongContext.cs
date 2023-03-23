5a50a54a853b47883d625a1aea57d7d53c881812
BeyondEternal <jojofan0606@gmail.com>
2023-03-23 00:59:50 -0600

    Agregar archivos de proyecto.

---
+++ SongListApp/Server/Data/SongContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SongListApp.Shared.Model;
+
+namespace SongListApp.Server.Data
+{
+    public class SongContext : DbContext
+    {
+        public SongContext(DbContextOptions<SongContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Song> Songs { get; set; }
+    }
+
+}
---
+++ SongListApp/Shared/Model/Song.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SongListApp.Shared.Model
+{
+    public class Song
+    {
+        public int Id { get; set; }
+        public string? Titulo { get; set; }
+        public string? Artista { get; set; }
+    }
+}
